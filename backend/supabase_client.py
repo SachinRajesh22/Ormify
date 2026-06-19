8806057e5523118080a77ca500cd4104997762b0
Sachin Rajesh <sachinrajesh2292005@gmail.com>
2026-06-19 13:29:43 +0530

    env updated

---
+++ backend/supabase_client.py
@@ -0,0 +1,13 @@
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_SERVICE_KEY")
+
+print("URL =", url)
+print("KEY EXISTS =", key is not None)
+
+supabase = create_client(url, key)
